9957dd288cb73c1da32276df7ec1f526fe2bf830
Shaszi <kamykpietras@gmail.com>
2025-01-10 11:36:49 +0100

    Code refactor. Added new functionality "remove student" and "remove course".

--- src/Shared/Domain/Entities/Student.cs
+++ src/Shared/Domain/Entities/Student.cs
@@ -1,23 +1,15 @@
-﻿namespace Domain.Entities
+﻿namespace Domain.Entities;
+
+public class Student
 {
-    public class Student
+    public string Name { get; set; }
+    public string StudentId { get; set; }
+    public List<Course> Courses { get; set; } = new();
+
+    public Student(string name, string studentId)
     {
-        public Student()
-        {
-            Name = string.Empty;
-            StudentId = string.Empty;
-            Courses = new List<Course>();
-        }
-
-        public Student(string name, string studentId)
-        {
-            Name = name;
-            StudentId = studentId;
-            Courses = new List<Course>();
-        }
-
-        public string Name { get; set; }
-        public string StudentId { get; set; }
-        public List<Course> Courses { get; set; }
+        Name = name;
+        StudentId = studentId;
+        Courses = new List<Course>();
     }
 }
